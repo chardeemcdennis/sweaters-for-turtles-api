04b60901a07452837672b58dd6c008b972f9b5be
james-coombs <coombsj4@yahoo.com>
2016-10-15 17:22:59 -0400

    Hotfix: Set mongoose uri
    Make mongoose uri point to enviormental secret if one is supplied.
    Add mlab support to heroku app.
    
    Still need to generate enviormental secrets for connecting mongoose
    to heroku app.

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,7 +1,14 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/sweaters-for-turtles-api';
+
+let uri;
+if (process.env.NODE_ENV === 'production') {
+  uri = process.env.MONGOLAB_URI;
+} else {
+  uri = 'mongodb://localhost/sweaters-for-turtles-api';
+}
+
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
