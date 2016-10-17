0c92544a58126467d85d95bff6e7f88bec474ffd
wmcb91 <wmcb91@gmail.com>
2016-10-17 15:42:11 -0400

    Add second option script for importing data.

--- scripts/products/import.sh
+++ scripts/products/import.sh
@@ -2,3 +2,6 @@
 # IMPORTANT, make sure your database has same name.
 
 mongoimport --db=sweaters-for-turtles-api --collection=products --type=csv --headerline --file=data/products.csv
+
+# First does not work, delete collection using db.products.drop() then use this code.
+mongoimport -d sweaters-for-turtles-api -c products --type csv --file data/products.csv --headerline
