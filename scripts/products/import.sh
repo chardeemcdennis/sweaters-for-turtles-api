60575357b5a3a9d5f3e63e439ee69d370b69bb39
wmcb91 <wmcb91@gmail.com>
2016-10-17 15:19:45 -0400

    Change db name in import script

--- scripts/products/import.sh
+++ scripts/products/import.sh
@@ -1,3 +1,4 @@
-mongoimport --db=mongo-crud --collection=products --type=csv --headerline --file=data/products.csv
-
 # Run from the api project root directory
+# IMPORTANT, make sure your database has same name.
+
+mongoimport --db=sweaters-for-turtles-api --collection=products --type=csv --headerline --file=data/products.csv
