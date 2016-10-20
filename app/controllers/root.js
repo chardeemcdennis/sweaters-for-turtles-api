b99c0d637841f2a2dc431644b836d48e4cc081c1
james-coombs <coombsj4@yahoo.com>
2016-10-19 23:17:34 -0400

    Rename App
    Change app display name when opening from heroku URL.

--- app/controllers/root.js
+++ app/controllers/root.js
@@ -6,7 +6,7 @@ const controller = require('lib/wiring/controller');
 const root = (req, res) => {
   res.json({
     index: {
-      title: 'Express Template',
+      title: 'Sweaters for Turtles API',
       environment: req.app.get('env'),
     },
   });
